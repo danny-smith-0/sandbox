05bd43f71ba01e3d301c9abaffb500be29ff0f68
Danny Smith <danny.smith.0@outlook.com>
2022-03-30 00:50:18 -0400

    Counts up and down between two limits until you're tired of it

--- condition_variables/source/cv.cpp
+++ condition_variables/source/cv.cpp
@@ -4,33 +4,53 @@
 
 std::mutex m;
 std::condition_variable cv;
+
 int value = 0;
-int count = 0;
+
+std::atomic_bool finished = false;
+int value_lower_limit = 1;
+int value_upper_limit = 5;
+int total_cycles = 3;
+int cycle_count = 0;
 
 void func1()
 {
-    std::unique_lock lock(m);
-    cv.wait(lock, [] { return value < 1;});
-    value++;
-    std::cout << "1: " << value << std::endl;
-    cv.notify_one();
+    while (!finished)
+    {
+        std::unique_lock lock(m);
+        cv.wait(lock, [] { return value <= value_lower_limit;});
+        while (value < value_upper_limit)
+        {
+            value++;
+            std::cout << "1: " << value << std::endl;
+        }
+        cv.notify_one();
+    }
 }
 
 void func2()
 {
-    std::unique_lock lock(m);
-    cv.wait(lock, [] { return value > 100;});
-    value -= 100;
-    std::cout << "2: " << value << std::endl;
-    count++;
-    cv.notify_one();
-    if (count >= 5)
-        int abc = 1;
+    while (!finished)
+    {
+        std::unique_lock lock(m);
+        cv.wait(lock, [] { return value >= value_upper_limit;});
+        while (value > value_lower_limit)
+        {
+            value -= 1;
+            std::cout << "2: " << value << std::endl;
+        }
+        cv.notify_one();
+        cycle_count++;
+        if (cycle_count >= total_cycles)
+            finished = true;
+    }
 }
 
 int main()
 {
     std::thread t1(func1);
     std::thread t2(func2);
+    t1.join();
+    t2.join();
     return 0;
 }
